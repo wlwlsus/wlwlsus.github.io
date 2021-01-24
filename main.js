627f4bf2df4d01e2152deed70bfa78b89030ae52
wonjun <159632_@naver.com>
2021-01-25 00:56:29 +0900

    Update nodejs

---
+++ main.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const app = http.createServer(function(request,response){
+    var _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    console.log(queryData);   
+
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+        response.writeHead(404);
+        // response.end();
+        // return;        
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + _url));
+ 
+});
+app.listen(3000);
+
+
+// var app = http.createServer(function(request,response){
+//   var url = request.url;
+//   if(request.url == '/'){
+//     url = '/index.html';
+//   }
+//   if(request.url == '/favicon.ico'){
+//       response.writeHead(404);
+//       response.end();
+//       return;        
+//   }
+//   response.writeHead(200);
+//   response.end(fs.readFileSync(__dirname + url));
+
+// });
+// app.listen(3000);
---
+++ syntax/template.js
@@ -0,0 +1,4 @@
+let word = "성원준";
+let letter = `Hello ${word}`;
+
+console.log(letter);
