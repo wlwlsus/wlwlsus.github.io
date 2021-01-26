76ad7e2db8c78aebf2341ddb20f9c0c3be1dc45b
wonjun <159632_@naver.com>
2021-01-26 18:29:08 +0900

    Add node js

--- main.js
+++ main.js
@@ -20,20 +20,3 @@ const app = http.createServer(function(request,response){
  
 });
 app.listen(3000);
-
-
-// var app = http.createServer(function(request,response){
-//   var url = request.url;
-//   if(request.url == '/'){
-//     url = '/index.html';
-//   }
-//   if(request.url == '/favicon.ico'){
-//       response.writeHead(404);
-//       response.end();
-//       return;        
-//   }
-//   response.writeHead(200);
-//   response.end(fs.readFileSync(__dirname + url));
-
-// });
-// app.listen(3000);
